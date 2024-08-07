e7d8516da13abfca4dedbdc22fcafcd7ca0bc64e
timhengc <2815145788@qq.com>
2024-08-07 21:54:46 +0800

    logo

--- packages/Webkul/Blog/src/Routes/shop-routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Webkul\Blog\Http\Controllers\Shop\BlogController;
-
-//Route::group(['middleware' => ['web', 'theme', 'locale', 'currency'], 'prefix' => 'blog'], function () {
-//    Route::get('', [BlogController::class, 'index'])->name('shop.blog.index');
-//});
