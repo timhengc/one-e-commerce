4e97b51eda7b7979bcc436eb5ef9b4536186f8dc
yexl <yexl23000@gmail.com>
2024-10-27 17:02:32 +0800

    update: try to parse markdown

--- packages/Webkul/Blog/src/Http/Controllers/Shop/BlogController.php
+++ packages/Webkul/Blog/src/Http/Controllers/Shop/BlogController.php
@@ -6,6 +6,9 @@ use Illuminate\Routing\Controller;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+use Parsedown;
+use Webbycrown\BlogBagisto\Models\Blog;
+
 class BlogController extends Controller
 {
     use DispatchesJobs, ValidatesRequests;
@@ -19,4 +22,16 @@ class BlogController extends Controller
     {
         return view('blog::shop.index');
     }
+
+    public function show($slug)
+{
+        $blogPost = Blog::where('slug', $slug)->firstOrFail();
+        $parsedown = new Parsedown();
+
+        $blogPost->description = $parsedown->text($blogPost->description);
+
+        return view('blog::shop.show', compact('blogPost'));
+    }
 }
+
+
